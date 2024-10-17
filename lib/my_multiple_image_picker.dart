69f3cc2e66403d814b70024b0dd6591c033a31ff
Renoldi <renoldiboz@gmail.com>
2024-10-17 15:36:02 +0700

    update

--- lib/my_multiple_image_picker.dart
+++ lib/my_multiple_image_picker.dart
@@ -160,7 +160,6 @@ class MultipleImagePickerComponent extends StatelessWidget {
                     if (onUploaded != null) {
                       onUploaded!(val);
                     }
-                    onChange!(controller);
                   });
                 },
                 onUploadFailed: onUploadFailed,
@@ -169,9 +168,23 @@ class MultipleImagePickerComponent extends StatelessWidget {
                     imagePickerPlaceHolderContainers.length - 1 >= index
                         ? imagePickerPlaceHolderContainers[index]
                         : null,
-                onChange: (val) {
-                  if (onChange != null) {
-                    onChange!(controller);
+                onChange: (ImagePickerController val) {
+                  if (showDescription) {
+                    Future.doWhile(() {
+                      if (val.value.state == ImagePickerComponentState.Enable) {
+                        return false;
+                      } else {
+                        return true;
+                      }
+                    }).then((value) {
+                      if (onChange != null) {
+                        onChange!(controller);
+                      }
+                    });
+                  } else {
+                    if (onChange != null) {
+                      onChange!(controller);
+                    }
                   }
                 },
                 showDescription: showDescription,
